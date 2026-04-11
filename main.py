acde360847c2b0a44a2a2a0e236ad83ec5e1338d
Scory237 <Scory237@gmail.com>
2026-04-12 00:09:38 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+pass = input("you're app password>>")
+master_pass = pass
+class Acc:
+  def __init__(self, username, password, app):
+    self.username = username
+    self.password = password
+    self.app = app
+  
